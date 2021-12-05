6d77b26c7bb8b5427eddf3ce4421bcb7edad8649
aydosenes <aydosenes@outlook.com>
2021-12-05 18:56:11 +0300

    init

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, TextInput, Button, Alert, View } from 'react-native';
+import {Audio} from 'expo-av';
 
 export default function App() {
+
+const [text, onChangeText] = React.useState("");
+
+const get = async () => {          
+  return fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${text}`)
+    .then((response) => response.json())
+    .then((json) => {          
+      console.log(json[0].phonetics[0].audio);
+      let audioUrl = `https:${json[0].phonetics[0].audio}`;
+      playSound(audioUrl);              
+    })
+    .catch(() => {
+      myAlert('Sorry.. We could not what you want.');
+    });     
+};
+
+const playSound = async (audioURL) => {
+  const sound = new Audio.Sound();
+  await sound.loadAsync(
+    { uri: audioURL},
+    { shouldPlay: true}
+  );
+  await sound.playAsync();
+};
+
+const myAlert = (msg) =>{
+  Alert.alert("Not Found", msg, [{text: "OK"}]);
+};
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text} 
+        /> 
+      <Button
+        title="search"
+        onPress={get}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +56,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    height: 40,
+    width: 200,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10
+  }
 });
